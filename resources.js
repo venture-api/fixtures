342ce2ed601e8d545b19adc7cac9f2d2cb6ef66f
Maxim Korinets <mkorinets@gmail.com>
2018-06-02 20:29:24 +0300

    Add ironOne resource

---
+++ resources.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ironOne: {
+        id: 'RS-O䠘힏B呹䅞B쎴䲪8T8賯䋸KS5O-CL',
+        producedAt: 'FC-WTQA6GN3DK27IT-GN',
+        ownerId: 'PL-BdiTUIO',
+        type: 'iron ore',
+        defects: ['moisture'],
+        location: 'FC-WTQA6GN3DK27IT-GN'
+    }
+};
