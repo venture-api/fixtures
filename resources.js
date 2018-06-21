052d1f051a5c0401c9b65dd8c20328de2ba9ef46
Maxim Korinets <mkorinets@gmail.com>
2018-06-21 09:44:01 +0300

    Fix ironOne id

--- resources.js
+++ resources.js
@@ -1,6 +1,6 @@
 module.exports = {
     ironOne: {
-        id: 'RS-O䠘힏B呹䅞B쎴䲪8T8賯䋸KS5O-CL',
+        id: 'RS-O䠘힏B呹䅞B쎴䲪8T8賯䋸KS5O-IO',
         producedAt: 'FC-WTQA6GN3DK27IT-GN',
         ownerId: 'PL-BdiTUIO',
         type: 'iron ore',
