ce0cbd5cfaa7a60fdc851a288f9227d426f9460a
Maksim Korinets <mkorinets@gmail.com>
2020-11-23 16:00:05 +0700

    Update collections, players

--- dictionary/collections.js
+++ dictionary/collections.js
@@ -3,5 +3,8 @@
 import w from './words.js';
 
 export default {
-    ACL: w.ACL
+    ACL: w.ACL,
+    player: w.player,
+    region: w.region,
+    resource: w.resource
 }
--- fixtures/players.js
+++ fixtures/players.js
@@ -1,12 +1,12 @@
 export default {
     bonner: {
-        email: 'FrederickKBonner@dayrep.com',
         name: 'Frederick K. Bonner',
-        id: 'PL-BdiTUIO'
+        id: 'PL-BdiTUIO',
+        externalId: '10769150350006150715113082367'  // something like `sub` from https://developers.google.com/identity/protocols/oauth2/openid-connect#an-id-tokens-payload
     },
     adams: {
-        email: 'JohnRAdams@armyspy.com',
         name: 'John R. Adams',
-        id: 'PL-AdiTjIO'
+        id: 'PL-AdiTjIO',
+        externalId: '20669150353336150715113082342'
     },
 };
